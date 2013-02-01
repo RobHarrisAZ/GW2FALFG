69cb7c3fb7f3f185f48403a80e8c9fb2b72d0a5f
RobHarrisAZ <monte96_@hotmail.com>
2013-01-31 20:54:22 -0700

    Initial Model object

---
+++ GW2FALFG.Web/GW2FALFG.Web/Models/GroupRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GW2FALFG.Web.Models
+{
+    public class GroupRequest
+    {
+        public int GroupRequestId { get; set; }
+        public string DungeonName { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public bool SpeedRunFl { get; set; }
+        public bool ExperiencedOnlyFl { get; set; }
+        public bool FullRunFl { get; set; }
+        public bool NewToDungeonFl { get; set; }
+        public int LookingForNumber { get; set; }
+        public string LanguagePreference { get; set; }
+        public int AgonyResistRequired { get; set; }
+    }
+}
