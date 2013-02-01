a0c766be59df8793d3ba1a9e0f7189575aae7f18
RobHarrisAZ <monte96_@hotmail.com>
2013-01-31 18:03:35 -0700

    Initial Project Setup

---
+++ GW2FALFG.Web/GW2FALFG.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GW2FALFG.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GW2FALFG.Web/GW2FALFG.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GW2FALFG.Web.WebApiApplication" Language="C#" %>
